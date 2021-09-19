722916129e601b7302cb696bd3fce286f044841d
Data-drone <bpl.law@gmail.com>
2021-09-19 23:46:04 +1000

    fixed local-path permissions switched minio to new drive path and aded auto creating of the minio folders needed

--- 4_deploy_minio.sh
+++ 4_deploy_minio.sh
@@ -2,7 +2,27 @@
 
 MINIO_SPACE=minio-tenant
 
+## Create folders for minio
+make_dir() {
+    if [[ ! -e $1 ]]; then
+        mkdir -p $1
+    elif [[ ! -d $1 ]]; then
+        echo "$1 already exists but is not a directory" 1>&2
+    fi
+}
 
+COMMON_STORE='/opt/data/common-storage'
+MINIO_DRIVE1='/opt/data/minio/drive1'
+MINIO_DRIVE2='/opt/data/minio/drive2'
+MINIO_DRIVE3='/opt/data/minio/drive3'
+MINIO_DRIVE4='/opt/data/minio/drive4'
+
+for FOLDER_PATH in $COMMON_STORE $MINIO_DRIVE1 $MINIO_DRIVE2 $MINIO_DRIVE3 $MINIO_DRIVE4
+do
+    make_dir $FOLDER_PATH
+done
+
+### Now that the folders we need are created, start the kube process
 kubectl create namespace ${MINIO_SPACE}
 
 echo "this requires installing minio operator plugin first and ensuring it is on path"
