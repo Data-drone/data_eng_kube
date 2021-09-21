1db2911dc7adf7ed59d6c834fdd207a9b73aa9f9
Data-drone <bpl.law@gmail.com>
2021-09-21 19:18:32 +1000

    adding ingress routine as a standard part of the build still need to look into why cuda is not working

--- 5_jupyterhub.sh
+++ 5_jupyterhub.sh
@@ -6,8 +6,8 @@ helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 
 helm repo update
 
-echo "we need to push the image from our local in"
-k3d image import -c test datadrone/deeplearn_minimal:cuda-11.1-base
+#echo "we need to push the image from our local in"
+#k3d image import -c test datadrone/deeplearn_minimal:cuda-11.1-base
 
 #echo "spark 3.1.1 with gpu image"
 #k3d image import -c test datadrone/spark-worker:3.1.1-hadoop3.2-rapids
@@ -25,6 +25,9 @@ helm upgrade --cleanup-on-fail \
 
 echo "adding service account for spawning spark workers"
 kubectl apply -f jupyterhub/spark-service-user.yaml
+
+echo "Apply Ingress script"
+kubectl apply -f jupyterhub/jupyter-ingress.yaml
 
 ### Notes from https://discourse.jupyter.org/t/jupyterhub-on-k8s-with-traefik/1605
 
