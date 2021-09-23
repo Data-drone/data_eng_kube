94a85971deb04d42247190f420907c934f1c0421
Data-drone <bpl.law@gmail.com>
2021-09-23 22:14:25 +1000

    update traefik to its own namespace

--- 1_custom_traefik.sh
+++ 1_custom_traefik.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+TRAEFIK_SPACE=traefik
+
 helm repo add traefik https://containous.github.io/traefik-helm-chart
 
 helm repo update
 
-helm install traefik traefik/traefik
+kubectl create namespace ${TRAEFIK_SPACE}
+
+helm install traefik traefik/traefik --namespace=${TRAEFIK_SPACE}
