7a4413bf240d394e9fb953baa0bb9b6bc31f6f3f
Data-drone <bpl.law@gmail.com>
2021-10-07 19:40:39 +1100

    fix localpath provision and set my custom configs

--- 3_local-path.sh
+++ 3_local-path.sh
@@ -2,6 +2,10 @@
 
 # We need local path for multiple things so lets break it out
 echo "deploy the local-path provisioner"
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+#kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+kubectl apply -f local-path-config/local-path-config.yaml
 
+kubectl apply -f local-path-config/role-local-path.yaml
+
+# Make local-path the default
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
