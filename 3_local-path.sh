09dbd7ffe6c80703c6ae8ad71bd742effcb23639
Data-drone <bpl.law@gmail.com>
2021-10-13 23:58:56 +1100

    removed the duplicated local-path role and binding in the kube-system namespace

--- 3_local-path.sh
+++ 3_local-path.sh
@@ -5,7 +5,8 @@ echo "deploy the local-path provisioner"
 #kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl apply -f local-path-config/local-path-config.yaml
 
-kubectl apply -f local-path-config/role-local-path.yaml
+# I don't think we need this anymore?
+#kubectl apply -f local-path-config/role-local-path.yaml
 
 # Make local-path the default
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
